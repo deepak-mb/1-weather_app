47b6499f0a3152077d0a7a009ab7c79cb20f5851
Deepak <deepak25mb@gmail.com>
2019-05-24 20:38:44 +0530

    Added global env variables and got api key

--- src/components/Layout/Image.js
+++ src/components/Layout/Image.js
@@ -5,7 +5,8 @@ export default function Image() {
     <div>
       <img
         src="https://66.media.tumblr.com/db3b68e5ccc2ebe25094b1e4c92aa0e2/tumblr_n0acluDfP31r5h04to1_500.gif"
-        style={{ "maxWidth": "100%" }}
+        alt="Mountains"
+        style={{ maxWidth: "100%" }}
       />
     </div>
   );
--- src/components/Layout/WeatherForm.js
+++ src/components/Layout/WeatherForm.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import Input from "./Input";
 import Button from "./Button";
+import axios from "axios";
+
+console.log(process.env.REACT_APP_WEATHER_API_KEY);
+
 
 class WeatherForm extends Component {
   state = {
@@ -14,9 +18,29 @@ class WeatherForm extends Component {
 
   onSubmit = e => {
     e.preventDefault();
-    console.log(this.state);
+    // const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+    const { city, country } = this.state;
+    this.getWeather(city, country);
   };
 
+  getWeather = async (city, country) => {
+    console.log(process.env.REACT_APP_WEATHER_API_KEY);
+    // Deepak key: c8ba6c107afe6c81bb0bc7bb6301b949
+    // Shante key: bc36f5b94179a769aae519f61e89018d
+    await axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=imperial`
+      )
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        if (err) {
+          alert("screwed");
+        }
+      });
+  };
   render() {
     const { city, country } = this.state;
     return (

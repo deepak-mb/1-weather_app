8db5d649e76fc953bad61f2b489aeba44cadef0a
Deepak <deepak25mb@gmail.com>
2019-05-25 11:32:58 +0530

    Updates

---
+++ precache-manifest.97158823469f915c4c809e94fb2fc31e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1336c00ad74a9489ca95921f3d77b113",
+    "url": "/1-weather_app/index.html"
+  },
+  {
+    "revision": "073e67df9bf7890e2e7d",
+    "url": "/1-weather_app/static/css/main.8d4a8645.chunk.css"
+  },
+  {
+    "revision": "7e42f927a57462577587",
+    "url": "/1-weather_app/static/js/2.4ed9389a.chunk.js"
+  },
+  {
+    "revision": "073e67df9bf7890e2e7d",
+    "url": "/1-weather_app/static/js/main.c2656819.chunk.js"
+  },
+  {
+    "revision": "75886001cfe7133a44a5",
+    "url": "/1-weather_app/static/js/runtime~main.b4d3172a.js"
+  }
+]);
